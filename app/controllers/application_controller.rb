cb34b4f5c388f5ad243064e115b7b090a92c228e
koushi6345 <k0929s0313@gmail.com>
2021-08-05 07:01:48 +0000

    public/staff finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    if current_admin
+      staffs_path
+    else
+      public_staff_path(current_staff.id)
+    end
+  end
+
 end
