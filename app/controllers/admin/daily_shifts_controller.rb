048f16ddb132b3ae95dc11e4dce917e6b359d77e
koushi6345 <k0929s0313@gmail.com>
2021-08-06 05:10:16 +0000

    admin/daily_shift first step

--- app/controllers/admin/daily_shifts_controller.rb
+++ app/controllers/admin/daily_shifts_controller.rb
@@ -1,2 +1,26 @@
 class Admin::DailyShiftsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+
+  def index
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def daily_shift_params
+  end
+  
 end
